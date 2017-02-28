5279f76ed9b21d25883643fea2cb9b183a32af5d
vladimir <>
2017-02-28 18:47:28 +0100

    adding framework7 and MainController, disabled spring security

--- src/main/java/com/ebanker/controller/MainController.java
+++ src/main/java/com/ebanker/controller/MainController.java
@@ -2,12 +2,18 @@ package com.ebanker.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
 
-    @RequestMapping("/login")
+    @RequestMapping(value = {"/", "/login"}, method = RequestMethod.GET)
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("/pocetna")
+    public String err() {
+        return "pocetna";
     }
 }
